f208ee02a0bb86a48ba6e562c9fb808c064ba8a9
sholanke precious <sholankeprecious3@gmail.com>
2026-03-27 11:36:13 +0100

    text

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Precious Beauty",
+  title: "SaloonBook  ",
   description: "Beauty link platform",
   icons: {
     icon: "/favicon.png",
